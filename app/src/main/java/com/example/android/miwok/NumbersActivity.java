3c415baa78b96a1371138c267c359ad460b985eb
Caesar <cezi8@vp.pl>
2018-03-18 18:46:01 +0100

    Add while loop to display TextViews in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -16,7 +16,7 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<>();
         words.set(0, "one");
         words.set(1, "two");
         words.set(2, "three");
@@ -29,45 +29,15 @@ public class NumbersActivity extends AppCompatActivity {
         words.set(9, "ten");
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-        TextView wordView = new TextView(this);
-        wordView.setText(words.get(0));
-        rootView.addView(wordView);
 
-        TextView wordView2 = new TextView(this);
-        wordView2.setText(words.get(1));
-        rootView.addView(wordView2);
+        int index = 0;
 
-        TextView wordView3 = new TextView(this);
-        wordView3.setText(words.get(2));
-        rootView.addView(wordView3);
-
-        TextView wordView4 = new TextView(this);
-        wordView4.setText(words.get(3));
-        rootView.addView(wordView4);
-
-        TextView wordView5 = new TextView(this);
-        wordView5.setText(words.get(4));
-        rootView.addView(wordView5);
-
-        TextView wordView6 = new TextView(this);
-        wordView6.setText(words.get(5));
-        rootView.addView(wordView6);
-
-        TextView wordView7 = new TextView(this);
-        wordView7.setText(words.get(6));
-        rootView.addView(wordView7);
-
-        TextView wordView8 = new TextView(this);
-        wordView8.setText(words.get(7));
-        rootView.addView(wordView8);
-
-        TextView wordView9 = new TextView(this);
-        wordView9.setText(words.get(8));
-        rootView.addView(wordView9);
-
-        TextView wordView10 = new TextView(this);
-        wordView10.setText(words.get(9));
-        rootView.addView(wordView10);
+        while(index<words.size()) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
 
         /*Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
         Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
